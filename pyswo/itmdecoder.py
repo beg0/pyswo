b34389b7de7e49b31d5cac762405e1d5314d412c
Cédric CARRÉE <beg0@free.fr>
2021-09-16 22:09:26 +0200

    First code for PySWO

---
+++ pyswo/itmdecoder.py
@@ -0,0 +1,397 @@
+# PYSWO - Copyright beg0 2021
+""" Decode ITM binary stream send over SWO """
+from __future__ import print_function
+
+from pyswo.itmpackets import (
+    ItmSwPacket,
+    ItmOverflowPacket,
+    ItmLocalTsPacket,
+    ItmGlobalTsPacket,
+    ItmDwtEventCounterPacket,
+    ItmExceptionEventPacket,
+    ItmPcSamplePacket,
+    ItmDwtPcPacket,
+    ItmDwtAddrOffsetPacket,
+    ItmDwtDataValuePacket,
+    ItmExtensionPacket,
+    )
+
+HEADER_SIZE=1
+MAX_PACKET_SIZE=5
+WORD_SIZE=4
+
+class ItmDecoder():
+    """ Parse stream of ITM binaries packets and output python object representing input packets"""
+    def __init__(self, feeder):
+        assert callable(feeder)
+
+        self.feeder = feeder
+        self.byte_stream = b''
+
+        self.sync_probe = b'\x00\x00\x00\x00\x00\x00'
+
+        self.synced = False
+
+
+    def __iter__(self):
+
+        new_data = self.feeder()
+        self.byte_stream += new_data
+        return self
+
+    def source_pkt_decoder(self):
+        """ Parse an ITM "source" packet, e.g. not a protocol packet.
+
+        Source packets are identified with the last 2 bits of header byte not null
+
+        There is 2 type of source packet, depending on value of bit #2:
+          - Case of Instrumentation packet (Software source, application)
+            Format is 0bAAAAA0SS, SS not 0b00
+            SS = size of payload
+            AAAAA = Source Address
+
+          - Case of Hardware packet (Hardware source, diagnostics)
+            Format is 0bAAAAA1SS, SS not 0b00
+            SS = size of payload
+            AAAAA = Packet type discriminator ID
+        """
+        header = self.byte_stream[0]
+
+        assert header & 0x03 != 0, "Not a ITM source packet"
+
+        # This is a SW packet
+        if header & 0x04 == 0:
+            return self.sw_source_pkt_decoder()
+
+        return self.hw_source_pkt_decoder()
+
+    def sw_source_pkt_decoder(self):
+        """ Extract ItmSwPacket() packet from ITM byte stream """
+
+        header = self.byte_stream[0]
+
+        assert header & 0x03 != 0, "Not a ITM source packet"
+        assert header & 0x04 == 0, "Not a ITM SW source packet"
+
+        payload_size =  (0, 1, 2, 4)[header & 0x03]
+        src_addr = header >> 3
+
+        packet_size = HEADER_SIZE + payload_size
+
+        # if we can't read the full payload,
+        if packet_size > len(self.byte_stream):
+            return (0, None)
+
+        payload = self.byte_stream[HEADER_SIZE:payload_size+HEADER_SIZE]
+
+        return (packet_size, ItmSwPacket(src_addr, payload))
+
+    def hw_source_pkt_decoder(self):
+        """ Extract ItmDwt*Packet() packet from ITM byte stream """
+
+        header = self.byte_stream[0]
+
+        assert header & 0x03 != 0, "Not a ITM source packet"
+        assert header & 0x04 != 0, "Not a ITM HW source packet"
+
+        payload_size =  (0, 1, 2, 4)[header & 0x03]
+        src_addr = header >> 3
+
+        packet_size = HEADER_SIZE + payload_size
+
+        # if we can't read the full payload,
+        if packet_size > len(self.byte_stream):
+            return (0, None)
+
+        payload = self.byte_stream[HEADER_SIZE:payload_size+HEADER_SIZE]
+
+        pkt=None
+        if src_addr == 0:
+            event = payload[0] & 0x2F
+            pkt=ItmDwtEventCounterPacket(event)
+        elif src_addr == 1:
+            exception_number = (payload[1] & 1) << 8 | payload[0]
+            event_type = (payload[1] >> 4) & 3
+            pkt=ItmExceptionEventPacket(exception_number, event_type)
+
+        # Periodic PC sample packets
+        elif  src_addr == 2:
+            program_counter = None
+            sleep = False
+            if len(payload) == 1:
+                sleep = True
+                program_counter = None
+            else:
+                sleep = False
+                program_counter = int.from_bytes(payload, "little")
+            pkt=ItmPcSamplePacket(program_counter, sleep)
+        else:
+            # Data trace packets
+            comp = int((src_addr & 0x6) >> 1)
+            value = int.from_bytes(payload, "little")
+            data_trace_pkt_id = src_addr & 0x19
+
+            if data_trace_pkt_id == 0x8:
+                pkt=ItmDwtPcPacket(
+                    comp=comp,
+                    program_counter=value)
+            if data_trace_pkt_id == 0x09:
+                pkt=ItmDwtAddrOffsetPacket(
+                    comp=comp,
+                    address_offset=value)
+            elif data_trace_pkt_id in (0x10, 0x11):
+                pkt = ItmDwtDataValuePacket(comp=comp,
+                    value=value,
+                    is_write=bool(data_trace_pkt_id & 0x01),
+                    size=len(payload))
+            else:
+                # other are reserved values, skip
+                self.synced = False
+                return(1, None)
+
+        assert pkt is not None, "Can't identify packet type with header %.8X" % header
+        return (packet_size, pkt)
+
+    @staticmethod
+    def is_continuation_bit_set(byte):
+        """ Tell if "Continuation bit" C is set """
+        return bool(byte & 0x80)
+
+    @staticmethod
+    def get_payload_with_continuation_byte(byte_stream):
+        """ extract a packet which has continuation bit"""
+
+        encoded_value = 0
+        payload_idx = 0
+        while payload_idx < WORD_SIZE:
+            # Enough data?
+            if len(byte_stream) <= HEADER_SIZE + payload_idx:
+                raise StopIteration
+
+            next_byte = byte_stream[HEADER_SIZE + payload_idx]
+
+            encoded_value |= (next_byte & 0x7F) << (payload_idx*7)
+            payload_idx += 1
+
+            if not ItmDecoder.is_continuation_bit_set(next_byte):
+                break
+
+        return (payload_idx, encoded_value)
+
+    def protocol_pkt_decoder(self):
+        """ Parse an ITM "protocol" packet, e.g. not a source packet.
+
+        A protocol packet has a header byte of 0bxxxxxx00, but not 0b00000000.
+        """
+
+        header = self.byte_stream[0]
+
+        assert header & 0x03 == 0 and header != 0, "Not a ITM protocol packet"
+
+        # ITM overflow
+        # Format is 0b01110000
+        # No payload
+        if header == 0x70:
+            return (1, ItmOverflowPacket())
+
+        # Local timestamp
+        # Format is 0bCDDD0000, DDD not 0b000 or 0b111
+        # D = Data, C = Continuation.
+        # Payload 0 to 4 bytes
+        if (header & 0x0F) == 0:
+            return self.local_ts_pkt_decoder()
+
+        # Global timestamp packet
+        # Format is 0b10T10100
+        # T = Global timestamp packet type.
+        # Payload 1 to 4 bytes
+        if (header & 0xDF) == 0x94:
+            return self.global_ts_pkt_decoder()
+
+        # Extension packets
+        # Format is 0bCDDD1S00
+        # S = Source, D = Data, C = Continuation.
+        if header & 0b00001000:
+            return self.extension_pkt_decoder()
+
+        # If we are here, it means we have a reserved packet
+        # 0b0xxx0100, 0bx1110000, 0b10x00100, 0b11xx0100,
+        # Just ignore it
+        self.synced = False
+        return (1, None)
+
+    def local_ts_pkt_decoder(self):
+        """ Extract ItmLocalTsPacket() packet from ITM byte stream """
+        header = self.byte_stream[0]
+
+        # Local timestamp
+        # Format is 0bCDDD0000, DDD not 0b000 or 0b111
+        # D = Data, C = Continuation.
+        # Payload 0 to 4 bytes
+
+        assert header & 0x0F == 0, "Not a Local Timestamp packet"
+
+        # Local timestamp packet format 1, two to five bytes
+        # Format is 0b 1 1    TC1   TC0  0    0    0    0
+        #           0b C TS6  TS5   TS4  TS3  TS2  TS1  TS0
+        #           0b C TS13 TS12  TS11 TS10 TS9  TS8  TS7
+        #           0b C TS20 TS19  TS18 TS17 TS16 TS15 TS14
+        #           0b 0 TS27 TS26  TS25 TS24 TS23 TS21 TS20
+        if ItmDecoder.is_continuation_bit_set(header):
+            time_control = int((header&0x30) >> 4)
+            timestamp = 0
+            try:
+                payload_size, timestamp  = \
+                    ItmDecoder.get_payload_with_continuation_byte(self.byte_stream)
+            except StopIteration:
+                # Not enough data
+                return (0, None)
+
+            pkt=ItmLocalTsPacket(timestamp=timestamp, time_control=time_control)
+
+        # Local timestamp packet format 2, single byte
+        # Format is 0b0TTT0000
+        else:
+            time_control = 0
+            timestamp = (header >> 4) & 0x07
+            payload_size = 0
+
+            # check for reserved values 0 & 7
+            # 0 would means it's part of a sync packet
+            # 7 would means it's part of a overflow packet
+            if timestamp in (0x00, 0x07):
+                self.synced = False
+                return (1, None)
+
+            pkt=ItmLocalTsPacket(timestamp, time_control)
+        return (HEADER_SIZE + payload_size, pkt)
+
+    def global_ts_pkt_decoder(self):
+        """ Extract ItmGlobalTsPacket() packet from ITM byte stream """
+
+        header = self.byte_stream[0]
+
+        # Global timestamp packet
+        # Format is 0b10T10100
+        # T = Global timestamp packet type.
+        # Payload 1 to 4 bytes
+        assert (header & 0xDF) == 0x94, "Not a Global Timestamp packet"
+
+        wrap=None
+        clock_change=None
+        gts_type = 1 if header & 0x20 else 2
+
+        try:
+            payload_size, timestamp  = \
+                ItmDecoder.get_payload_with_continuation_byte(self.byte_stream)
+        except StopIteration:
+            # Not enough data
+            return (0, None)
+
+        if gts_type == 1:
+            # For GTS type 1, bits 26 and 27 has special meanings
+            clock_change = bool((timestamp & 0x4000000)>>26)
+            wrap = bool((timestamp & 0x8000000)>>27)
+            timestamp = timestamp & 0xC000000
+
+        pkt=ItmGlobalTsPacket(gts_type, timestamp, wrap=wrap, clock_change=clock_change)
+        return (HEADER_SIZE + payload_size, pkt)
+
+    def extension_pkt_decoder(self):
+        """ Extract ItmExtensionPacket() packet from ITM byte stream """
+
+        header = self.byte_stream[0]
+
+        # Extension packets
+        # Format is 0bCDDD1S00
+        # S = Source, D = Data, C = Continuation.
+        assert header & 0b00001000, "Not an extension packet"
+
+        first_ext_bits = (header & 0x70) >> 4
+        source = bool(header & 0x04 >> 2)
+        if not ItmDecoder.is_continuation_bit_set(header):
+            extension_info = int(first_ext_bits)
+            pkt = ItmExtensionPacket(source, extension_info)
+            return (HEADER_SIZE, pkt)
+
+        try:
+            payload_size, last_ext_bits  = \
+                ItmDecoder.get_payload_with_continuation_byte(self.byte_stream)
+        except StopIteration:
+            # Not enough data
+            return (0, None)
+
+        extension_info = last_ext_bits<<3 | first_ext_bits
+        pkt = ItmExtensionPacket(source, extension_info)
+        return (HEADER_SIZE + payload_size, pkt)
+
+    def __next__(self):
+
+        while self.byte_stream:
+
+            self.sync_probe = self.sync_probe[1:] + self.byte_stream[0:1]
+
+            if self.sync_probe == b'\x00\x00\x00\x00\x00\x80':
+                self.synced = True
+                self.byte_stream = self.byte_stream[1:]
+                continue
+
+            #if not self.synced:
+            #    self.byte_stream = self.byte_stream[1:]
+            #    continue
+
+            # Parse header, on one byte
+            header = self.byte_stream[0]
+
+            # SYNC packet
+            if header == 0:
+                self.synced = False
+                self.byte_stream = self.byte_stream[1:]
+                continue
+
+
+            if header & 0x03:
+                pkt_decoder=self.source_pkt_decoder
+            else:
+                pkt_decoder=self.protocol_pkt_decoder
+
+            (consumed_len, packet) = pkt_decoder()
+
+            if consumed_len > 0:
+                self.byte_stream = self.byte_stream[consumed_len:]
+
+            if packet is not None:
+                return packet
+
+            # If above decoder was not able to consume a single byte,
+            # it means we don't have enough data to decode a new packet
+            # stop iteration now
+            if consumed_len == 0:
+                raise StopIteration
+
+        # Buffer empty
+        raise StopIteration
+
+
+if __name__ == "__main__":
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("localhost", 50003))
+
+    def itm_feeder():
+        """ Read ITM stream from TCP connection """
+        data = []
+        while not data:
+            data = s.recv(1024)
+            if not data:
+                print("reconnect")
+                s.shutdown(socket.SHUT_RDWR)
+                s.connect(("localhost", 50003))
+        return data
+
+
+    decoder = ItmDecoder(itm_feeder)
+
+    while True:
+        for itmPacket in decoder:
+            print(itmPacket)
---
+++ pyswo/itmpackets.py
@@ -0,0 +1,230 @@
+# PYSWO - Copyright beg0 2021
+""" Python class to represent decoded ITM packets """
+
+import enum
+
+MAX_PACKET_SIZE=5
+
+class ItmTimeControl(enum.IntEnum):
+    """ Values for Time Control (TC) field in ITM timestamp packets """
+    ITC_SYNC = 0
+    ITC_DELAYED_DATA = 1
+    ITC_DELAYED_EVENT = 2
+    ITC_DELAYED_DATA_EVENT = 3
+
+class ItmGtsType(enum.IntEnum):
+    """ Values for types of ITM Global Typestamp packets """
+    IGTS_1 = 1
+    IGTS_2 = 2
+    IGTS_LOWER_BITS = 1
+    IGTS_UPPER_BITS = 2
+
+class ItmDwtCounterWrap(enum.IntFlag):
+    """DWT Event counter packet payload bit assignments """
+    IDCW_CYC = 0x20
+    IDCW_FOLD = 0x10
+    IDCW_LSU = 0x08
+    IDCW_SLEEP = 0x04
+    IDCW_EXE = 0x02
+    IDCW_CPI = 0x01
+
+class ItmDwtExceptionEvent(enum.IntEnum):
+    """ Action taken by the processor when it enters, exits, or returns to an exception """
+    IDEE_RSVD = 0
+    IDEE_ENTER = 0x01
+    IDEE_EXIT = 0x02
+    IDEE_RETURN = 0x03
+
+class ItmPacket():
+    """ Base class for all ITM packets """
+
+class ItmSourcePacket(ItmPacket):
+    """ Base class for all ITM "source" packets, e.g. an Instrumentation packet or a Hardware
+    source packet.
+
+    Source packets have a header byte of 0bxxxxxxSS, where SS is not 0b00. They always have
+    a payload of 1, 2 or 4 bytes.
+    """
+
+class ItmProtocolPacket(ItmPacket):
+    """ Base class for all ITM "protocol" packets
+
+    Protocol packets have a header byte of 0bxxxxxxSS, where SS is 0b00 but header byte is not
+    0b00000000.
+    """
+
+class ItmDwtEventCounterPacket(ItmSourcePacket):
+    """ ITM DWT Event Counter packet
+
+    The DWT unit generates an Event counter packet when a counter value wraps round to zero, that
+    is, when:
+
+        - a countup, or incrementing, counter overflows
+        - a countdown, or decrementing, counter underflows.
+
+    The packet has a single payload byte, containing a set of bits that show which counters have
+    wrapped. Typically a single counter wraps, however the DWT can generate this packet with
+    multiple payload bits set to 1, indicating a combination of counters wrapping to zero.
+    """
+    def __init__(self, event):
+        self.event = ItmDwtCounterWrap(event)
+
+    def __repr__(self):
+        return "<ItmDwtEventCounterPacket(event=%r)>" % (self.event)
+
+class ItmExceptionEventPacket(ItmSourcePacket):
+    """ ITM Exception Event packet
+
+    Issued by DWT when the core enter an exception state
+    """
+    def __init__(self, exception_number, event_type):
+        self.exception_number = exception_number
+        self.event_type = ItmDwtExceptionEvent(event_type)
+
+    def __repr__(self):
+        return "<ItmExceptionEventPacket(exception_number=%d, event_type=%r>" % \
+            (self.exception_number, self.event_type)
+
+class ItmPcSamplePacket(ItmSourcePacket):
+    """ ITM PC Sampling packet
+
+    Issued by DWT at regular interval
+    """
+    def __init__(self, program_counter, sleep=False):
+        self.program_counter = program_counter
+        self.sleep = sleep
+    def __repr__(self):
+        if self.sleep:
+            return "<ItmPcSamplePacket(sleep=%r)>" % self.sleep
+        # otherwise it's a packet with a PC
+        return "<ItmPcSamplePacket(program_counter=0x%.8x)>" % self.program_counter
+
+class ItmDwtPcPacket(ItmSourcePacket):
+    """ ITM DWT Data trace PC value packet
+
+    Issued by DWT when a comparator in DWT is trigger
+    """
+
+    def __init__(self, comp, program_counter):
+        self.comp = comp
+        self.program_counter = program_counter
+
+    def __repr__(self):
+        return "<ItmDataTracePcValue(comp=%d, PC=%.8x)>" % (self.comp, self.program_counter)
+
+class ItmDwtAddrOffsetPacket(ItmSourcePacket):
+    """ ITM DWT Data trace address offset packet
+
+    Issued by DWT when a comparator in DWT is trigger
+    """
+
+    def __init__(self, comp, address_offset):
+        self.comp = comp
+        self.address_offset = address_offset
+
+    def __repr__(self):
+        return "<ItmDwtAddrOffsetPacket(comp=%d, addr_offset=%.8x)>" % \
+            (self.comp, self.address_offset)
+
+class ItmDwtDataValuePacket(ItmSourcePacket):
+    """ ITM DWT Data value packet
+
+    Issued by DWT when a comparator in DWT is trigger
+    """
+
+    def __init__(self, comp, value, is_write, size):
+        self.comp = comp
+        self.value = value
+        self.size = size
+        self.is_write = is_write
+
+    def __repr__(self):
+        return "<ItmDwtDataValuePacket(comp=%d, value=%x (%d bits), write=%s)>" % \
+            (self.comp, self.value, self.size*8, self.is_write)
+
+class ItmSwPacket(ItmSourcePacket):
+    """ ITM Software packet
+
+    Packet used for instrumentation. Issued when software write the corresponding stimulis port
+    """
+    def __init__(self, channel, payload):
+        self.channel = channel
+        self.payload = payload
+
+    def __repr__(self):
+        return "<ItmSwPacket(channel=%d, payload=%r)>" % (self.channel, self.payload)
+
+class ItmOverflowPacket(ItmProtocolPacket):
+    """ ITM Overflow packet
+
+    The ITM outputs an Overflow packet if:
+
+        - software writes to a Stimulus Port register when the stimulus port output buffer is full
+        - the DWT attempts to generate a Hardware source packet when the DWT output buffer is full
+        - the Local timestamp counter overflows.
+    """
+
+class ItmLocalTsPacket(ItmProtocolPacket):
+    """ ITM Local Timestamp packet
+
+    A Local timestamp packet encodes timestamp information, for generic control and
+    synchronization, based on a timestamp counter in the ITM. To reduce the trace bandwidth:
+
+        - The local timestamping scheme uses delta timestamps, meaning each local timestamp value
+          gives the interval since the generation of the previous Local timestamp packet.
+        - The Local timestamp packet length, 1-5 bytes, depends on the required timestamp value.
+
+    Whenever the ITM outputs a Local timestamp packet, it clears its timestamp counter to zero.
+    """
+    def __init__(self, timestamp, time_control):
+        self.timestamp = timestamp
+        self.time_control = ItmTimeControl(time_control)
+
+    def __repr__(self):
+        return "<ItmLocalTsPacket(timestamp=%d, time_control=%s (0x%x))" % \
+            (self.timestamp, self.time_control.name, self.time_control.value)
+
+class ItmGlobalTsPacket(ItmProtocolPacket):
+    """ ITM Global Timestamp packet
+
+    If an implementation supports global timestamping, the ITM generates Global timestamp packets
+    based on a global timestamp clock. A full global timestamp is a 48-bit value. This means
+    global timestamping uses two timestamp packet formats:
+
+        - GTS1 packets transmit bits [25:0] of the timestamp value, and the ITM compresses these
+          by not transmitting high-order bytes that are unchanged from the previous timestamp value.
+        - GTS2 packets transmit bits [47:26] of the timestamp values, and when a GTS2 packet is
+          required the ITM always transmits it in full, as a five-byte packet.
+    """
+    def __init__(self, gts_type, timestamp, wrap=None, clock_change=None):
+        self.gts_type=ItmGtsType(gts_type)
+        self.timestamp=timestamp
+        self.wrap=wrap
+        self.clock_change=clock_change
+
+        assert (self.gts_type == ItmGtsType.IGTS_1 and self.wrap is not None) or \
+            ((self.gts_type == ItmGtsType.IGTS_2 and self.wrap is None)), \
+            "wrap can only be set with GTS type 1"
+
+        assert (self.gts_type == ItmGtsType.IGTS_1 and self.clock_change is not None) or \
+            ((self.gts_type == ItmGtsType.IGTS_2 and self.clock_change is None)), \
+                "clock_change can only be set with GTS type 1"
+
+    def __repr__(self):
+        if self.gts_type == ItmGtsType.IGTS_1:
+            return "<ItmGlobalTsPacket(type=%s, timestamp=%d, wrap=%r, clock_change=%r)" % \
+                (self.gts_type, self.timestamp, self.wrap, self.clock_change)
+
+        return "<ItmGlobalTsPacket(type=%s, timestamp=%d)" % (self.gts_type, self.timestamp)
+
+class ItmExtensionPacket(ItmProtocolPacket):
+    """ ITM Extension packet
+
+    An Extension packet provides additional information about the identified source.
+    """
+    def __init__(self, source, extension_info):
+        self.source = source
+        self.extension_info = extension_info
+
+    def __repr__(self):
+        return "<ItmExtensionPacket(source=%r, payload=%r)>" % (self.source, self.extension_info)
