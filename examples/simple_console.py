d83ea6fd1c1ed7c35f57131620e1799ccf92836e
Cédric CARRÉE <beg0@free.fr>
2022-06-27 17:06:40 +0200

    simple_console: re-wrote as class
    It's more pythonic ;)

--- examples/simple_console.py
+++ examples/simple_console.py
@@ -67,92 +67,136 @@ def  cmp_range(range1, range2):
     # Then sort by (inverted) range (e.g. larger ranges comes first)
     return range2[1] - range1[1]
 
-cli_parser = argparse.ArgumentParser(description=__doc__)
-cli_parser.add_argument("-c", "--swo-channel",
-                        type=int,
-                        nargs='*',
-                        default=[0],
-                        help="SWO channel to display")
-cli_parser.add_argument("-o", "--output-file",
-                        type=argparse.FileType('wb'),
-                        default=DEFAULT_OUTPUT_STREAM,
-                        help="Save console to file")
+class App():
+    """ Main class for application """
 
-pattern_matching_group = cli_parser.add_argument_group("pattern matching")
-pattern_matching_group.add_argument("--encoding",
-                                    type=encoding,
-                                    default='latin',
-                                    help="String encoding (only used for regexp lookup")
-pattern_matching_group.add_argument("-p", "--print",
-                                    choices=[
-                                        PRINT_ALL,
-                                        PRINT_MATCH_LINE,
-                                        PRINT_MATCH_PATTERN,
-                                        PRINT_MATCH_GROUPS,
-                                        PRINT_MATCH_PATTERN_AND_GROUPS,
-                                    ],
-                                    default=PRINT_ALL,
-                                    help="control what to display")
-pattern_matching_group.add_argument("--match-group-separator",
-                                    default=";",
-                                    help="separator to use when --print is one of %s" % \
-                                        ', '.join([
-                                            PRINT_MATCH_PATTERN,
-                                            PRINT_MATCH_GROUPS,
-                                            PRINT_MATCH_PATTERN_AND_GROUPS
-                                        ]))
-pattern_matching_group.add_argument("-r", "--regexp",
-                                    type=compiled_pcre,
-                                    action='append',
-                                    help="text (as PCRE) to look for")
+    @staticmethod
+    def build_argparse():
+        """ Generate the Command Line argument parser using argparse """
+        cli_parser = argparse.ArgumentParser(description=__doc__)
+        cli_parser.add_argument("-c", "--swo-channel",
+                                type=int,
+                                nargs='*',
+                                default=[0],
+                                help="SWO channel to display")
+        cli_parser.add_argument("-o", "--output-file",
+                                type=argparse.FileType('wb'),
+                                default=DEFAULT_OUTPUT_STREAM,
+                                help="Save console to file")
 
-add_all_feeders_to_argparse(cli_parser)
+        pattern_matching_group = cli_parser.add_argument_group("pattern matching")
+        pattern_matching_group.add_argument("--encoding",
+                                            type=encoding,
+                                            default='latin',
+                                            help="String encoding (only used for regexp lookup")
+        pattern_matching_group.add_argument("-p", "--print",
+                                            choices=[
+                                                PRINT_ALL,
+                                                PRINT_MATCH_LINE,
+                                                PRINT_MATCH_PATTERN,
+                                                PRINT_MATCH_GROUPS,
+                                                PRINT_MATCH_PATTERN_AND_GROUPS,
+                                            ],
+                                            default=PRINT_ALL,
+                                            help="control what to display")
+        pattern_matching_group.add_argument("--match-group-separator",
+                                            default=";",
+                                            help="separator to use when --print is one of %s" % \
+                                                ', '.join([
+                                                    PRINT_MATCH_PATTERN,
+                                                    PRINT_MATCH_GROUPS,
+                                                    PRINT_MATCH_PATTERN_AND_GROUPS
+                                                ]))
+        pattern_matching_group.add_argument("-r", "--regexp",
+                                            type=compiled_pcre,
+                                            action='append',
+                                            help="text (as PCRE) to look for")
 
-config = cli_parser.parse_args()
+        add_all_feeders_to_argparse(cli_parser)
 
-try:
-    swo_feeder = config.feeder_generator.create()
-except AttributeError:
-    cli_parser.error("No SWO input stream defined")
+        return cli_parser
 
-decoder = ItmDecoder(swo_feeder)
+    def __init__(self):
+        cli_parser = App.build_argparse()
 
-log_line = b''
+        self.config = cli_parser.parse_args()
 
-patterns = config.regexp or []
-while True:
-    for itm_packet in decoder:
-        if isinstance(itm_packet, ItmSwPacket) and itm_packet.channel in config.swo_channel:
-            if config.print == PRINT_ALL:
-                config.output_file.write(itm_packet.payload)
+        try:
+            swo_feeder = self.config.feeder_generator.create()
+        except AttributeError:
+            cli_parser.error("No SWO input stream defined")
 
-            log_line += itm_packet.payload
-            if itm_packet.payload == b'\n':
-                log_line_str = log_line.decode(config.encoding)
-                matches = []
-                for pattern in patterns:
-                    matches += list(pattern.finditer(log_line_str))
+        self.decoder = ItmDecoder(swo_feeder)
 
-                if matches:
-                    if config.print == PRINT_MATCH_LINE:
-                        config.output_file.write(log_line)
-                    elif config.print in [PRINT_MATCH_PATTERN, PRINT_MATCH_GROUPS, PRINT_MATCH_PATTERN_AND_GROUPS]:
+        self.patterns = self.config.regexp or []
 
-                        include_group0 = config.print in [PRINT_MATCH_PATTERN, PRINT_MATCH_PATTERN_AND_GROUPS]
-                        include_other_groups = config.print in [PRINT_MATCH_GROUPS, PRINT_MATCH_PATTERN_AND_GROUPS]
-                        flat_groups = []
-                        for compare_result in matches:
-                            if include_group0:
-                                flat_groups.append((compare_result.regs[0], compare_result.group(0)))
-                            if include_other_groups:
-                                for pos, txt in zip(compare_result.regs[1:], compare_result.groups()):
-                                    flat_groups.append((pos, txt))
+        self.log_line = b''
 
-                        flat_groups.sort(key=functools.cmp_to_key(
-                            lambda pos_txt1, pos_txt2: cmp_range(pos_txt1[0], pos_txt2[0])))
 
-                        flat_captured_txt = [pos_txt[1] for pos_txt in flat_groups]
-                        output_str = config.match_group_separator.join(flat_captured_txt) + '\n'
-                        config.output_file.write(output_str.encode(config.encoding))
+    def run(self):
+        """ Application main loop """
 
-                log_line = b''
+        self.log_line = b''
+
+        try:
+            print("Start reading SWO stream. Press CTRL-C to exit")
+            while True:
+                for itm_packet in self.decoder:
+                    if isinstance(itm_packet, ItmSwPacket) and \
+                        itm_packet.channel in self.config.swo_channel:
+                        self.handle_sw_packet(itm_packet)
+        except KeyboardInterrupt:
+            print("Capture interrupted by user")
+        except EOFError:
+            print("End of SWO stream")
+        return 0
+
+    def handle_sw_packet(self, itm_packet):
+        """ Manage SWO packet ItmSwPacket """
+        config = self.config # Alias for lazy people
+
+        if config.print == PRINT_ALL:
+            config.output_file.write(itm_packet.payload)
+
+        self.log_line += itm_packet.payload
+        if itm_packet.payload == b'\n':
+            log_line_str = self.log_line.decode(config.encoding)
+            matches = []
+            for pattern in self.patterns:
+                matches += list(pattern.finditer(log_line_str))
+
+            if matches:
+                if config.print == PRINT_MATCH_LINE:
+                    config.output_file.write(self.log_line)
+                elif config.print in [
+                                      PRINT_MATCH_PATTERN,
+                                      PRINT_MATCH_GROUPS,
+                                      PRINT_MATCH_PATTERN_AND_GROUPS
+                                      ]:
+
+                    include_group0 = config.print in [
+                                                      PRINT_MATCH_PATTERN,
+                                                      PRINT_MATCH_PATTERN_AND_GROUPS
+                                                      ]
+                    include_other_groups = config.print in [
+                                                            PRINT_MATCH_GROUPS,
+                                                            PRINT_MATCH_PATTERN_AND_GROUPS
+                                                           ]
+                    flat_groups = []
+                    for compare_result in matches:
+                        if include_group0:
+                            flat_groups.append((compare_result.regs[0], compare_result.group(0)))
+                        if include_other_groups:
+                            for pos, txt in zip(compare_result.regs[1:], compare_result.groups()):
+                                flat_groups.append((pos, txt))
+
+                    flat_groups.sort(key=functools.cmp_to_key(
+                        lambda pos_txt1, pos_txt2: cmp_range(pos_txt1[0], pos_txt2[0])))
+
+                    flat_captured_txt = [pos_txt[1] for pos_txt in flat_groups]
+                    output_str = config.match_group_separator.join(flat_captured_txt) + '\n'
+                    config.output_file.write(output_str.encode(config.encoding))
+
+            self.log_line = b''
+
+sys.exit(App().run())
