ad8027464ea492ce27e0687aeaf8c317fb85198b
Cédric CARRÉE <beg0@free.fr>
2022-06-28 18:04:49 +0200

    Profiler: rename most variables to be full snaky_case

--- examples/profiler.py
+++ examples/profiler.py
@@ -43,18 +43,18 @@ def main():
 
     config = parser.parse_args()
 
-    elffile = ELFFile(config.PROGRAM)
+    elf_file = ELFFile(config.PROGRAM)
 
-    if not elffile.has_dwarf_info():
+    if not elf_file.has_dwarf_info():
         print('  file has no DWARF info')
 
-    dwarfinfo = elffile.get_dwarf_info()
+    dwarf_info = elf_file.get_dwarf_info()
     swo_feeder_generator = getattr(config, 'feeder_generator', None)
     if not swo_feeder_generator:
         parser.error('No SWO stream configured')
     swo_feeder = swo_feeder_generator.create()
 
-    itmdecoder = ItmDecoder(feeder=swo_feeder)
+    itm_decoder = ItmDecoder(feeder=swo_feeder)
 
     sleep_counter = 0
     pc_usage = defaultdict(lambda:0)
@@ -63,7 +63,7 @@ def main():
         print("Start reading SWO stream. Press CTRL-C to exit")
 
         while True:
-            for pkt in itmdecoder:
+            for pkt in itm_decoder:
                 if isinstance(pkt, ItmPcSamplePacket):
                     total_pc_sample += 1
                     if pkt.sleep:
@@ -80,13 +80,13 @@ def main():
     print(f"sleep= {sleep_counter}")
     #print(f"pc= {pc_usage}")
 
-    print_hottest_functions(elffile, dwarfinfo, pc_usage, total_pc_sample)
+    print_hottest_functions(elf_file, dwarf_info, pc_usage, total_pc_sample)
 
-    print_hottest_lines(dwarfinfo, pc_usage, total_pc_sample)
+    print_hottest_lines(dwarf_info, pc_usage, total_pc_sample)
 
-def print_hottest_functions(elffile, dwarfinfo, pc_usage, total_pc_sample):
+def print_hottest_functions(elf_file, dwarf_info, pc_usage, total_pc_sample):
     """ Display the function that use the most the MPU"""
-    func_usage = get_usage_by_func(elffile, dwarfinfo, pc_usage)
+    func_usage = get_usage_by_func(elf_file, dwarf_info, pc_usage)
 
     #print(f"func_usage={func_usage}")
 
@@ -106,73 +106,73 @@ def print_hottest_functions(elffile, dwarfinfo, pc_usage, total_pc_sample):
         print(f"{func_name_latin:20s}: {percentage_usage:.2f}%")
     print("")
 
-def print_hottest_lines(dwarfinfo, pc_usage, total_pc_sample):
+def print_hottest_lines(dwarf_info, pc_usage, total_pc_sample):
     """ Display the source code line that use the most the MPU"""
 
     print("Hottest lines")
-    fileline_usage = get_usage_by_file_line(dwarfinfo, pc_usage)
-    sorted_fileline_usage = sorted(fileline_usage,
-                                   key=lambda fileline: fileline_usage[fileline],
+    file_line_usage = get_usage_by_file_line(dwarf_info, pc_usage)
+    sorted_file_line_usage = sorted(file_line_usage,
+                                   key=lambda file_line: file_line_usage[file_line],
                                    reverse=True)
-    for fileline in sorted_fileline_usage[0:10]:
+    for file_line in sorted_file_line_usage[0:10]:
 
-        percentage_usage = 100.0*fileline_usage[fileline]/total_pc_sample
-        print(f"{fileline:20s}: {percentage_usage:.2f}%")
+        percentage_usage = 100.0*file_line_usage[file_line]/total_pc_sample
+        print(f"{file_line:20s}: {percentage_usage:.2f}%")
 
-def get_usage_by_func(elffile, dwarfinfo, pc_usage):
+def get_usage_by_func(elf_file, dwarf_info, pc_usage):
     """ Get CPU usage per function"""
     func_usage = defaultdict(lambda: 0)
-    fnr = FuncNameRegistry(dwarfinfo)
+    fnr = FuncNameRegistry(dwarf_info)
 
-    func_symbols = get_all_func_symbols(elffile)
+    func_symbols = get_all_func_symbols(elf_file)
     func_symbols.sort(key=lambda x: x['address'])
     func_addresses = [x['address'] for x in func_symbols]
 
     for pc in pc_usage:
-        funcname = fnr.find_func(pc)
+        func_name = fnr.find_func(pc)
 
         # if we can't find info in DWARF, fallback to symbols table
-        if funcname is None:
+        if func_name is None:
             idx = bisect.bisect(func_addresses, pc)
             if idx == 0:
-                funcname = f"*unknown* ({pc:8x})"
+                func_name = f"*unknown* ({pc:8x})"
             else:
                 nearest_func = func_symbols[idx - 1]
                 nearest_func_name = nearest_func['name']
                 nearest_func_offset = pc-nearest_func['address']
-                funcname = f"<{nearest_func_name}+{nearest_func_offset:x}>"
+                func_name = f"<{nearest_func_name}+{nearest_func_offset:x}>"
 
-        func_usage[funcname] += pc_usage[pc]
+        func_usage[func_name] += pc_usage[pc]
     return func_usage
 
-def get_usage_by_file_line(dwarfinfo, pc_usage):
+def get_usage_by_file_line(dwarf_info, pc_usage):
     """ Get CPU usage per source code line"""
     line_usage = defaultdict(lambda: 0)
-    flr = FileLineRegistry(dwarfinfo)
+    flr = FileLineRegistry(dwarf_info)
 
     for pc in pc_usage:
         file, line = flr.find_file_line(pc)
 
-        fileline = ""
+        file_line = ""
         if file is None:
-            fileline = f"*unknown* ({pc:8x})"
+            file_line = f"*unknown* ({pc:8x})"
         elif line is None:
-            fileline = file
+            file_line = file
         else:
-            fileline = str(file, 'utf8') + ":" + str(line)
+            file_line = str(file, 'utf8') + ":" + str(line)
 
-        line_usage[fileline] += pc_usage[pc]
+        line_usage[file_line] += pc_usage[pc]
     return line_usage
 class FuncNameRegistry():
     """ Find function names from DWARF debug info """
-    def __init__(self, dwarfinfo = None, skip_pc_zero=True):
+    def __init__(self, dwarf_info = None, skip_pc_zero=True):
         self.func_infos = []
-        self.func_lowpc = []
+        self.func_low_pc = []
 
-        if dwarfinfo:
-            self.update_dwarf_info(dwarfinfo, skip_pc_zero=skip_pc_zero)
+        if dwarf_info:
+            self.update_dwarf_info(dwarf_info, skip_pc_zero=skip_pc_zero)
 
-    def update_dwarf_info(self, dwarfinfo, skip_pc_zero=True):
+    def update_dwarf_info(self, dwarf_info, skip_pc_zero=True):
         """ Update function names from DWARF info"""
 
         func_infos = []
@@ -180,12 +180,12 @@ class FuncNameRegistry():
         # entry with an address range that includes the given address. Note that
         # this simplifies things by disregarding subprograms that may have
         # split address ranges.
-        for CU in dwarfinfo.iter_CUs():
+        for CU in dwarf_info.iter_CUs():
             for DIE in CU.iter_DIEs():
                 try:
                     if DIE.tag == 'DW_TAG_subprogram':
-                        lowpc = DIE.attributes['DW_AT_low_pc'].value
-                        if skip_pc_zero and lowpc == 0:
+                        low_pc = DIE.attributes['DW_AT_low_pc'].value
+                        if skip_pc_zero and low_pc == 0:
                             continue
 
                         # DWARF v4 in section 2.17 describes how to interpret the
@@ -193,108 +193,108 @@ class FuncNameRegistry():
                         # For class 'address' it's taken as an absolute address
                         # (similarly to DW_AT_low_pc); for class 'constant', it's
                         # an offset from DW_AT_low_pc.
-                        highpc_attr = DIE.attributes['DW_AT_high_pc']
-                        highpc_attr_class = describe_form_class(highpc_attr.form)
-                        if highpc_attr_class == 'address':
-                            highpc = highpc_attr.value
-                        elif highpc_attr_class == 'constant':
-                            highpc = lowpc + highpc_attr.value
+                        high_pc_attr = DIE.attributes['DW_AT_high_pc']
+                        high_pc_attr_class = describe_form_class(high_pc_attr.form)
+                        if high_pc_attr_class == 'address':
+                            high_pc = high_pc_attr.value
+                        elif high_pc_attr_class == 'constant':
+                            high_pc = low_pc + high_pc_attr.value
                         else:
                             print('Error: invalid DW_AT_high_pc class:',
-                                highpc_attr_class)
+                                high_pc_attr_class)
                             continue
 
                         func_infos.append(
                             {
-                                'lowpc': lowpc,
-                                'highpc': highpc,
+                                'low_pc': low_pc,
+                                'high_pc': high_pc,
                                 'func_name': DIE.attributes['DW_AT_name'].value
                             }
                         )
                 except KeyError:
                     continue
 
-        self.func_infos = sorted(func_infos, key=lambda x: x['lowpc'])
-        self.func_lowpc = [ x['lowpc'] for x in self.func_infos ]
+        self.func_infos = sorted(func_infos, key=lambda x: x['low_pc'])
+        self.func_low_pc = [ x['low_pc'] for x in self.func_infos ]
 
     def find_func(self, address):
         """ Resolve function name from code address """
-        idx = bisect.bisect_right(self.func_lowpc, address)
+        idx = bisect.bisect_right(self.func_low_pc, address)
         if idx == 0:
             return None
         func_info = self.func_infos[idx - 1]
-        if func_info['lowpc'] <= address < func_info['highpc']:
+        if func_info['low_pc'] <= address < func_info['high_pc']:
             return func_info['func_name']
         return None
 
 class FileLineRegistry():
     """ Find source code line of a PC address using DWARF debug info """
 
-    def __init__(self, dwarfinfo = None, skip_pc_zero=True):
+    def __init__(self, dwarf_info = None, skip_pc_zero=True):
         self.file_line_infos = []
-        self.file_line_lowpc = []
+        self.file_line_low_pc = []
 
-        if dwarfinfo:
-            self.update_dwarf_info(dwarfinfo, skip_pc_zero=skip_pc_zero)
+        if dwarf_info:
+            self.update_dwarf_info(dwarf_info, skip_pc_zero=skip_pc_zero)
 
-    def update_dwarf_info(self, dwarfinfo, skip_pc_zero=True):
+    def update_dwarf_info(self, dwarf_info, skip_pc_zero=True):
         """ Update source code line info from DWARF info"""
         file_line_infos=[]
         # Go over all the line programs in the DWARF information, looking for
         # one that describes the given address.
-        for CU in dwarfinfo.iter_CUs():
+        for CU in dwarf_info.iter_CUs():
             # First, look at line programs to find the file/line for the address
-            lineprog = dwarfinfo.line_program_for_CU(CU)
-            prevstate = None
-            for entry in lineprog.get_entries():
+            line_prog = dwarf_info.line_program_for_CU(CU)
+            prev_state = None
+            for entry in line_prog.get_entries():
                 # We're interested in those entries where a new state is assigned
                 if entry.state is None:
                     continue
                 # Looking for a range of addresses in two consecutive states that
                 # contain the required address.
-                if prevstate and not (skip_pc_zero and prevstate.address == 0):
+                if prev_state and not (skip_pc_zero and prev_state.address == 0):
                     file_line_infos.append(
                         {
-                            'lowpc': prevstate.address,
-                            'highpc': entry.state.address,
-                            'filename': lineprog['file_entry'][prevstate.file - 1].name,
-                            'line': prevstate.line
+                            'low_pc': prev_state.address,
+                            'high_pc': entry.state.address,
+                            'filename': line_prog['file_entry'][prev_state.file - 1].name,
+                            'line': prev_state.line
                         }
                     )
                 if entry.state.end_sequence:
                     # For the state with `end_sequence`, `address` means the address
                     # of the first byte after the target machine instruction
                     # sequence and other information is meaningless. We clear
-                    # prevstate so that it's not used in the next iteration. Address
+                    # prev_state so that it's not used in the next iteration. Address
                     # info is used in the above comparison to see if we need to use
-                    # the line information for the prevstate.
-                    prevstate = None
+                    # the line information for the prev_state.
+                    prev_state = None
                 else:
-                    prevstate = entry.state
-        self.file_line_infos = sorted(file_line_infos, key=lambda x: x['lowpc'])
-        self.file_line_lowpc = [ x['lowpc'] for x in self.file_line_infos ]
+                    prev_state = entry.state
+        self.file_line_infos = sorted(file_line_infos, key=lambda x: x['low_pc'])
+        self.file_line_low_pc = [ x['low_pc'] for x in self.file_line_infos ]
 
     def find_file_line(self, address):
         """ Resolve source code line from code address """
-        idx = bisect.bisect_right(self.file_line_lowpc, address)
+        idx = bisect.bisect_right(self.file_line_low_pc, address)
         if idx == 0:
             return None, None
         func_info = self.file_line_infos[idx - 1]
-        if func_info['lowpc'] <= address < func_info['highpc']:
+        if func_info['low_pc'] <= address < func_info['high_pc']:
             return (func_info['filename'], func_info['line'])
         return None, None
 
-def get_all_func_symbols(elffile):
+def get_all_func_symbols(elf_file):
     """ Extract all symbols related to functions from an ELF file
 
     These symbols are a good indicator of where a function start.
     """
     func_symbols = []
-    symtab_section=elffile.get_section_by_name(".symtab")
-    if not symtab_section:
+    sym_tab_section=elf_file.get_section_by_name(".symtab")
+    if not sym_tab_section:
         return []
 
-    for symbol in symtab_section.iter_symbols():
+    for symbol in sym_tab_section.iter_symbols():
         try:
             if symbol['st_info']['type'] == 'STT_FUNC':
                 func_symbols.append({'name': symbol.name, 'address': symbol['st_value']})
